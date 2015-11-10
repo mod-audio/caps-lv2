091475ced3f02494f3ec2dda6f7fd9026982d184
falkTX <falktx@gmail.com>
2015-11-10 17:17:39 +0100

    Add interface.cc files

---
+++ plugins/mod-caps-Noisegate.lv2/interface.cc
@@ -0,0 +1,31 @@
+
+#include "version.h"
+#include "basics.h"
+
+#include "Cabinet.h"
+#include "Chorus.h"
+#include "Phaser.h"
+#include "Sin.h"
+#include "Fractals.h"
+#include "Reverb.h"
+#include "Compress.h"
+#include "Click.h"
+#include "Eq.h"
+#include "Saturate.h"
+#include "White.h"
+#include "AutoFilter.h"
+#include "Amp.h"
+#include "Pan.h"
+#include "Scape.h"
+#include "ToneStack.h"
+#include "Noisegate.h"
+
+#include "Descriptor.h"
+
+LV2_SYMBOL_EXPORT
+const LV2_Descriptor *
+lv2_descriptor(uint32_t i)
+{
+    static const Descriptor<Noisegate> lv2_descriptor(CAPS_URI "Noisegate");
+    return i == 0 ? &lv2_descriptor : 0;
+}
